bfea6d4a76106fde88bb5288236bda6ea3be554b
Romulo AC <rcianci@gmail.com>
2018-08-26 02:12:12 -0300

    Migração para versão 4.10

--- Models/CorreiosShippingModel.cs
+++ Models/CorreiosShippingModel.cs
@@ -1,5 +1,5 @@
-﻿using Nop.Web.Framework.Mvc.ModelBinding;
-using Nop.Web.Framework.Mvc.Models;
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
 using System.Collections.Generic;
 
 namespace NopBrasil.Plugin.Shipping.Correios.Models
